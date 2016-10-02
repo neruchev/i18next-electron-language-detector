2bdc609ce9328547ba3804e9e4ee3b8119da5019
Cyril Neruchev <menandr@list.ru>
2016-10-02 17:52:12 +0300

    Arrow function was removed.

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const app = require('electron').app;
 module.exports = {
   init: Function.prototype,
   type: 'languageDetector',
-  detect: () => app.getLocale(),
+  detect: function() {
+    return app.getLocale();
+  },
   cacheUserLanguage: Function.prototype,
 };
