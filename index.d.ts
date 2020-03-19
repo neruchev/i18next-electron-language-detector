d8f8de280e3887057f345e1f4098300f84b3973e
Kirill Neruchev <kirill@neruchev.com>
2020-03-19 19:26:56 +0100

    Update package coding style

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,8 @@
 interface languageDetector {
-    'init': Function,
-    'type': Function,
-    'detect': string,
-    'cacheUserLanguage': Function
+  init: Function;
+  type: Function;
+  detect: string;
+  cacheUserLanguage: Function;
 }
 
 declare const LanguageDetector: languageDetector;
